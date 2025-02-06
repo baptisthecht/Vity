21c9491ce9d3e782a3edc68d861b3688d0d1293c
Baptist HECHT <121203663+baptisthecht@users.noreply.github.com>
2025-02-06 17:07:32 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,22 @@ const font = DM_Sans({
 export const metadata: Metadata = {
   title: "Vity",
   description: "Developed by X",
+  icons: {
+    icon: { rel: "shortcut icon", url: "/favicon.ico", type: "image/x-icon" },
+    apple: [
+      { url: "/apple-touch-icon.png" },
+      { url: "/apple-touch-icon-57x57.png", sizes: "57x57" },
+      { url: "/apple-touch-icon-72x72.png", sizes: "72x72" },
+      { url: "/apple-touch-icon-76x76.png", sizes: "76x76" },
+      { url: "/apple-touch-icon-114x114.png", sizes: "114x114" },
+      { url: "/apple-touch-icon-120x120.png", sizes: "120x120" },
+      { url: "/apple-touch-icon-144x144.png", sizes: "144x144" },
+      { url: "/apple-touch-icon-152x152.png", sizes: "152x152" },
+      { url: "/apple-touch-icon-180x180.png", sizes: "180x180" },
+    ],
+  },
 };
+
 
 export default function RootLayout({
   children,
